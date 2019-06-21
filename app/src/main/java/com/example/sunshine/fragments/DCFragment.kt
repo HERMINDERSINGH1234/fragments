9548e9e27172294a0fd8ed4cd7156344ac81270d
HERMINDER SINGH <hermindersingh12345@gmail.com>
2019-06-21 16:53:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sunshine/fragments/DCFragment.kt
@@ -0,0 +1,37 @@
+package com.example.sunshine.fragments
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_marvel.view.marvelLv
+
+
+class DCFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_marvel, container, false)
+
+        val name = arguments?.getString("NAME")
+        if (name == "DC")
+            view.marvelLv.adapter = ArrayAdapter<String>(
+                requireContext(), android.R.layout.simple_list_item_1,
+                arrayOf("Wonder Woman", "Superman", "Aquaman", "Batman")
+            )
+        else if(name == "MARVEL")
+            view.marvelLv.adapter = ArrayAdapter<String>(
+                requireContext(), android.R.layout.simple_list_item_1,
+                arrayOf("Iron Man","Captain America","Thor","Black Panther")
+            )
+        return view
+    }
+
+
+}
---
+++ app/src/main/java/com/example/sunshine/fragments/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.sunshine.fragments
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.dc
+import kotlinx.android.synthetic.main.activity_main.marvel
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        dc.setOnClickListener {
+
+            val bundle = Bundle()
+            var frag = DCFragment()
+            bundle.putString("NAME", "DC")
+
+            frag.arguments = bundle
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, frag)
+                .commit()
+        }
+        marvel.setOnClickListener {
+
+            val bundle = Bundle()
+            val frag = DCFragment()
+            bundle.putString("NAME", "MARVEL")
+
+            frag.arguments = bundle
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, frag)
+                .commit()
+        }
+
+        //        supportFragmentManager.beginTransaction()
+//            .replace(R.id.container, DCFragment())
+//            .commit()
+
+
+    }
+
+}
